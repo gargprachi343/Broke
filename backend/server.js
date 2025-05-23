4081729f2815e3080d8a6fdc5a80cf46652184ae
Lavanya Varshney <149246272+Lavavarshney@users.noreply.github.com>
2025-05-23 21:34:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'Uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
  
   app.use('/api/auth', loginRoutes);
 
