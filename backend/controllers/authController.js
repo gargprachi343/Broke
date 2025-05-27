95df53e30337ee32de727c3c8ff27526164d22a5
Lavanya Varshney <149246272+Lavavarshney@users.noreply.github.com>
2025-05-27 17:24:37 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,8 +12,8 @@ exports.signup = async (req, res) => {
       confirmEmail,
       password,
       confirmPassword,
-      username,
-      collegeName,
+      username
+ 
     } = req.body;
 
     // Basic validations
@@ -22,8 +22,8 @@ exports.signup = async (req, res) => {
       !confirmEmail ||
       !password ||
       !confirmPassword ||
-      !username ||
-      !collegeName
+      !username 
+    
     ) {
       return res.status(400).json({ message: "All fields are required" });
     }
